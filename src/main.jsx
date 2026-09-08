9adebf87f25008894c73f7e1933da4417722171e
aofazlarn-dev <aofazlarn@gmail.com>
2026-09-08 19:47:59 +0700

    Restyle family note as formal gold bordered invitation

--- src/main.jsx
+++ src/main.jsx
@@ -5,4 +5,5 @@ import './styles.css';
 import './luxury.css';
 import './ivory.css';
 import './mockup.css';
+import './note.css';
 createRoot(document.getElementById('root')).render(<React.StrictMode><App /></React.StrictMode>);
